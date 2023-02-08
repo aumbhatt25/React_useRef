c2562c730e36f5df8a6de2d967ade23998e74333
= <=>
2023-02-08 10:49:43 +0530

    8feb commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [value, setValue] = useState("");
+  // const [value, setValue] = useState("");
+  const value = "";
+
   const valueRef = useRef();
   console.log("render");
   const handleClick = () => {
     console.log(valueRef);
-    setValue(valueRef.current.value);
+    // setValue(valueRef.current.value);
+
+    value = valueRef.current.value;
   };
   return (
     <div className="App">
+      {/* {valueRef.current ? (valueRef.current.value) : ""} */}
       <h4>Value: {value}</h4>
       <input ref={valueRef} />
       <button onClick={handleClick}>click</button>
+
+      {/* <input onChange={handleClick} ref={valueRef} /> */}
     </div>);
 }
 
